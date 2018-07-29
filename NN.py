a47190126872d08889cd1bbefdf601b4558abfac
karlis <karlis.vigulis@city.ac.uk>
2018-07-29 17:33:05 +0100

    Softmax

--- NN.py
+++ NN.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from utils import *
 
+
+
 DATA_DIR = 'cifar-10-batches-py'
 DATA_BATCH = 'data_batch_1'
 LABEL_MAP_FILE = 'batches.meta'
@@ -23,15 +25,35 @@ def apply_reLU(input):
     return a
 
 
+def softmax(x):
+    # Apply column-wise Softmax to an array
+    x=x.astype(float)
+    if x.ndim==1:
+        S=np.sum(np.exp(x))
+        return np.exp(x)/S
+    elif x.ndim==2:
+        result=np.zeros_like(x)
+        M,N=x.shape
+        for n in range(N):
+            S=np.sum(np.exp(x[:,n]))
+            result[:,n]=np.exp(x[:,n])/S
+        return result
+    else:
+        print("The input array is not 1- or 2-dimensional.")
+
 
 class NN_layer():
-    def __init__(self, input_size, layer_size):
+    def __init__(self, input_size, layer_size, softmax_output=False):
         # Use standard normal distribution to initialise the weights and biases.
         # And apply the vanishing gradient prevention trick [ *np.sqrt(2/input_size) ].
         self.w = np.random.randn(layer_size, input_size)*np.sqrt(2/input_size)
         self.b = (np.random.randn(layer_size)*np.sqrt(2/input_size)).reshape(layer_size,1)
+        self.z = np.zeros(layer_size)  # Derivative of activation function with respect to z.
         self.da_dz = np.zeros(layer_size) # Derivative of activation function with respect to z.
 
+
+        # Makes a logits layer
+        self.softmax_output = softmax_output
 
     def calculate_layer_activations(self, input):
         '''
@@ -40,42 +62,36 @@ class NN_layer():
         :return:
         '''
 
-        print("w.x", self.w.dot(input.T))
-        print("b", self.b)
-        z = np.add(self.w.dot(input.T), self.b)
+        print("w", self.w.shape)
+        print("b", self.b.shape)
+        print("input", input.shape)
+        print("self.w.T.dot(input)", self.w.dot(input).shape)
+        self.z = np.add(self.w.dot(input), self.b) # This matrix will also be useful in backpropagation.
+        print("self.z", self.z.shape)
+        a = apply_reLU(self.z)
+
+        if self.softmax_output:
+            a = softmax(a)
+
+        return a
 
 
-        a = apply_reLU(z)
 
+def crossEntropyLoss():
+    pass
+
+
+class BackProp():
+
+    def __int__(self):
+        # Update the derivatives of the nodes.
         self.da_dz[a > 0] = 1
-        print("a", a)
         print("da/dz", self.da_dz)
 
-        return a
-
-
-
-class Output_layer():
-    # Network's output layer
-    def __init__(self, number_of_classes):
-        self.output_nodes = np.zeros(number_of_classes)
-
-    def calculate_layer_activations(self, input):
-        '''
-        Return the output values of the current layer, given an input
-        :param input: activations of the previous layer and the values of input layer
-        :return:
-        '''
-        z = self.w.dot(input) + self.b
-        a = apply_reLU(z)
-
-        return a
-
-
-test_input = np.array([[2,1,4],[3,6,-1]])
+test_input = np.array([[2,1,4],[3,3,-1],[1,3,8]]).T
 layer1 = NN_layer(3, 4)
 
-output = NN_layer(4, 4)
+output = NN_layer(4, 3, softmax_output=True)
 
 
 print(test_input)
@@ -87,6 +103,10 @@ print(layer1.b)
 activation_l1 = layer1.calculate_layer_activations(test_input)
 activation_output = output.calculate_layer_activations(activation_l1)
 
+
+
+
+print("Output:", activation_output)
 
 
 for x, y in zip(input_dict[b'data'][:3], input_dict[b'labels'][:3]):
